8dd2f76eb11d0b14cdeecfe78d1436f0cfd3bdee
Simona <trajkovskasimona10@gmail.com>
2024-05-16 15:12:16 +0200

    Eloquent relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
     use HasFactory;
-    protected $table='jobs_listings';
+    protected $table='job_listings';
     protected $fillable=['title', 'salary'];
+
+    public function employer(): BelongsTo 
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags()  
+    {
+        return $this->belongsToMany(Tag::class,foreignPivotKey:"job_listing_id");
+    }
    
 }
