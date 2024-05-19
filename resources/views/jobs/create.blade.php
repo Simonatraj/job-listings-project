1b79b14641901879216933aaeb72b4b5125cc934
Simona <trajkovskasimona10@gmail.com>
2024-05-20 00:06:09 +0200

    client and server-side validation

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -4,8 +4,8 @@
     </x-slot:heading>
 
     <form method="POST" action="/jobs">
-        @csrf 
-        
+        @csrf
+
         <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
                 <h2 class="text-base font-semibold leading-7 text-gray-900">Create a new Job</h2>
@@ -19,10 +19,14 @@
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input type="text" name="title" id="title"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="Shift Leader">
+                                    placeholder="Shift Leader" required>
                             </div>
                         </div>
+                        @error('title')
+                        <p class="text-xs text-red-500 font-semibold mt-1">{{$message}}</p>
+                        @enderror
                     </div>
+                 
 
                     <div class="sm:col-span-4">
                         <label for="salary" class="block text-sm font-medium leading-6 text-gray-900">Salary</label>
@@ -31,12 +35,27 @@
                                 class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                                 <input type="text" name="salary" id="salary"
                                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                    placeholder="$20 000USD per Year">
+                                    placeholder="$20 000USD per Year" required>
                             </div>
                         </div>
+                        @error('salary')
+                        <p class="text-xs text-red-500 font-semibold mt-1">{{$message}}</p>
+                        @enderror
                     </div>
+               
                 </div>
+                {{-- <div class="mt-10">
+                    @if ($errors->any())
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li class="text-red-500 italic">{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                </div> --}}
+
             </div>
+
 
             <div class="mt-6 flex items-center justify-end gap-x-6">
                 <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
